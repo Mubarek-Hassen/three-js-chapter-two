5214ec6d682bf53aaf668c3bc162dcc6f18d9b88
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-10-12 23:50:19 -0500

    spotLight added

--- src/shadows.js
+++ src/shadows.js
@@ -25,13 +25,13 @@ gui.add(directionalLight, 'intensity').min(0).max(3).step(0.001)
 gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
 gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
-
 directionalLight.castShadow = true
 
+//* RENDER SIZE OPTIMIZATION
 directionalLight.shadow.mapSize.width = 1024
 directionalLight.shadow.mapSize.height = 1024
 
-
+//* AMPLITUDE/SHADOW CAMERA OPTIMIZATION
 directionalLight.shadow.camera.top = 2
 directionalLight.shadow.camera.right = 2
 directionalLight.shadow.camera.bottom = -2
@@ -39,10 +39,19 @@ directionalLight.shadow.camera.left = -2
 directionalLight.shadow.camera.near = 1
 directionalLight.shadow.camera.far = 6
 
+//* BLUR OPTIMIZATION
+directionalLight.shadow.radius = 3
+
 const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
 directionalLightCameraHelper.visible = false
 
-scene.add(directionalLight, directionalLightCameraHelper)
+// scene.add(directionalLight, directionalLightCameraHelper)
+
+//! SPOTLIGHT
+const spotLight = new THREE.SpotLight(0xffffff, 2, 10, Math.PI * 0.3)
+spotLight.castShadow = true;
+spotLight.position.set(0,2,2)
+scene.add(spotLight, spotLight.target)
 
 //! MATERIALS
 const material = new THREE.MeshStandardMaterial()
@@ -87,6 +96,9 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.outputColorSpace = THREE.LinearSRGBColorSpace
 renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+
 //! CLOCK
 const clock = new THREE.Clock()
 
