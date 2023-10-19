dba912da1b20b8cd42f660b5fa06ca8bbb82616b
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-10-18 23:09:27 -0500

    graves added

--- src/haunted-house.js
+++ src/haunted-house.js
@@ -79,6 +79,28 @@ bush4.position.set(-1, 0.05, 2.6)
 bush4.scale.set(0.15, 0.15, 0.15)
 
 house.add(bush1, bush2, bush3, bush4)
+
+//* GRAVES
+const graves = new THREE.Group()
+scene.add(graves)
+
+const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2)
+const graveMaterial = new THREE.MeshStandardMaterial({ color: "#b2b6b1" })
+
+
+
+for (let i = 0; i < 50; i++){
+  const angle = Math.random() * Math.PI * 2
+  const radius = 4 + (Math.random() * 5)
+  const x = Math.sin(angle) * radius
+  const z = Math.cos(angle) * radius
+  const grave = new THREE.Mesh(graveGeometry, graveMaterial)
+  grave.position.set(x, 0.4, z)
+  grave.rotation.y = (Math.random() - 0.5) * 0.4
+  grave.rotation.z = (Math.random() - 0.5) * 0.4
+  graves.add(grave)
+}
+
 //! LIGHTS
 //* Ambient light
 const ambientLight = new THREE.AmbientLight('#ffffff', 1)
@@ -102,7 +124,7 @@ const sizes = {
 
 //! CAMERA
 const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 1, 1000)
-camera.position.set(4, 2, 5)
+camera.position.set(0, 15, 15)
 scene.add(camera)
 
 //* resize & full screen
