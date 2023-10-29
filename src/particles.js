04c04aac4178c8cf6e8d35d5949de3bfea2df2dc
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-10-28 21:51:26 -0500

    particles sphere added

--- src/particles.js
+++ src/particles.js
@@ -2,10 +2,29 @@ import * as THREE from "three"
 import * as dat from "lil-gui"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 
+//! GUI
+// const gui = new dat.GUI()
 //! CANVAS
 const canvas = document.querySelector("canvas.webgl")
 //! SCENE
 const scene = new THREE.Scene()
+
+//! TEXTURES
+const textureLoader = new THREE.TextureLoader()
+
+//! OBJECTS
+
+//* PARTICLES
+const particlesGeometry = new THREE.SphereGeometry(1,32,32)
+const particlesMaterial = new THREE.PointsMaterial({
+  size: 0.02,
+  sizeAttenuation: true,
+})
+
+//* particles points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
 
 //! CAMERA
 const sizes = {
@@ -15,6 +34,26 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 1, 1000)
 camera.position.set(0, 1, 3)
 scene.add(camera)
+
+//* resize & full screen
+window.addEventListener("resize", ()=>{
+  //  update the sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  //  update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+  //  update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+window.addEventListener("dblclick", ()=>{
+  if(!document.fullscreenElement){
+    canvas.requestFullscreen()
+  } else{
+    document.exitFullscreen()
+  }
+})
 
 //! CONTROLS
 const controls = new OrbitControls(camera, canvas)
