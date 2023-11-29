0b90486387f2de2afb1ec96cae1837df4522e274
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-11-28 20:35:03 -0500

    particles added

--- src/scroll-based-animation.js
+++ src/scroll-based-animation.js
@@ -90,6 +90,27 @@ scene.add(mesh1, mesh2, mesh3)
 const sectionMeshes = [ mesh1, mesh2, mesh3 ]
 
 
+//!		PARTICLES
+const particleCount = 200
+const positions = new Float32Array(particleCount * 3)
+
+for (let i = 0; i < particleCount; i++) {
+	positions[i * 3 + 0] = (Math.random() - 0.5) * 10
+	positions[i * 3 + 1] = objectsDistance * 0.5 - Math.random() * objectsDistance * 3
+	positions[i * 3 + 2] = (Math.random() - 0.5) * 10
+}
+const particlesGeometry = new THREE.BufferGeometry()
+particlesGeometry.setAttribute("position", new THREE.BufferAttribute(positions, 3))
+
+const particlesMaterial = new THREE.PointsMaterial({
+	color: parameters.materialColor,
+	sizeAttenuation: true,
+	size: 0.03
+})
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+
+scene.add(particles)
 
 //!		CAMERA
 const cameraGroup = new THREE.Group()
@@ -133,19 +154,24 @@ window.addEventListener("mousemove", (event)=>{
 
 //!		ANIMATE
 const clock = new THREE.Clock()
+let prevTime = 0
 
 function tick(){
 	const elapsedTime = clock.getElapsedTime()
+	const deltaTime = elapsedTime - prevTime
+	prevTime = elapsedTime
+
+
+
 
 	// update objects
-
 	camera.position.y = - scrollY / sizes.height * objectsDistance
 
-	const parallaxX = cursor.x
-	const parallaxY = - cursor.y
+	const parallaxX = cursor.x * 0.5
+	const parallaxY = - cursor.y * 0.5
 
-	cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 0.1
-	cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 0.1
+	cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 0.01 * deltaTime
+	cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 0.01 * deltaTime
 
 	for(const mesh of sectionMeshes){
 		
